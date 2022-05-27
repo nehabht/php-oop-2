bdecd1d489646b8086d6fba33b9dd69d56975917
nehabht <ne.bhatti2@icloud.com>
2022-05-27 15:02:24 +0200

    add user fix

--- Models/User.php
+++ Models/User.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once __DIR__ . "/Models/User.php";
 
 class User {
     
--- index.php
+++ index.php
@@ -12,6 +12,6 @@ require_once __DIR__ . "/Models/User.php";
 // $ball = new Toys('Ball', '10.00', '1X433', '100','Dog');
 // var_dump($ball);
 
-$neha = new User('neha', 'neha@gmail.com', '19202934440', 'yes');
+$neha = new User('neha', 'neha@gmail.com', '19202934440', "yes");
 var_dump($neha);
-var_dump($neha->discountOption(yes));
+var_dump($neha->discountOption(true));
